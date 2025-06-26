dbd5b446fb67e5bcc01bcef032af935a325be001
Hayden Sullivan <shsullivan2@gmail.com>
2025-06-25 23:18:11 -0400

    First Commit

---
+++ src/test/java/RakerTest.java
@@ -0,0 +1,58 @@
+class RakerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addDisc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFromFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeDisc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void listAllDiscs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void listReturnedDiscs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void listSoldDiscs() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByPhone() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateReturnedValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateSoldValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void returnDisc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sellDisc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateContactInformation() {
+    }
+}
